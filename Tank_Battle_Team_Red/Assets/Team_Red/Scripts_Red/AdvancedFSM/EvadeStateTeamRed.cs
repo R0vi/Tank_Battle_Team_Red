9ce060147f4c83bcb8fa5e00e686beed1ab104dd
Valerie de Bie <valerie-debie@hotmail.com>
2020-10-15 11:56:28 +0200

    Added script for evade state (empty)

---
+++ Tank_Battle_Team_Red/Assets/Team_Red/Scripts_Red/AdvancedFSM/EvadeStateTeamRed.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvadeStateTeamRed : FSMStateTeamRed
+{
+    public EvadeStateTeamRed()
+    {
+        stateIdTeamRed = FSMStateIDTeamRed.Evade;
+    }
+    public override void ReasonTeamRed(Transform redTank, IList<Transform> platoonRedTanks, IList<Transform> enemyTanks)
+    {
+
+    }
+
+    public override void ActTeamRed(Transform redTank, IList<Transform> platoonRedTanks, IList<Transform> enemyTanks)
+    {
+        //Do Nothing for the evade state
+    }
+}
