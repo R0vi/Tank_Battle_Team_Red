4f63485befe453d41b5041d6337f973f4d0178e4
Sander Bussink <s.bussink@dimerce.com>
2020-10-26 15:36:16 +0100

    Refactor

--- Tank_Battle_Team_Red/Assets/Team_Red/Scripts_Red/AdvancedFSM/FSMStateTeamRed.cs
+++ Tank_Battle_Team_Red/Assets/Team_Red/Scripts_Red/AdvancedFSM/FSMStateTeamRed.cs
@@ -35,16 +35,14 @@ public abstract class FSMStateTeamRed
 
     private static DataTeamRed GetDataTeamRed()
     {
-        string[] guids = UnityEditor.AssetDatabase.FindAssets("t:Data Team Red");
+        var guids = UnityEditor.AssetDatabase.FindAssets("t:Data Team Red");
         if (guids.Length == 0)
         {
             return ScriptableObject.CreateInstance<DataTeamRed>();
         }
-        else
-        {
-            var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
-            return UnityEditor.AssetDatabase.LoadAssetAtPath<DataTeamRed>(path);
-        }
+
+        var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
+        return UnityEditor.AssetDatabase.LoadAssetAtPath<DataTeamRed>(path);
     }
 
     public void AddTransitionTeamRed(Transition transition, FSMStateIDTeamRed idTeamRed)
