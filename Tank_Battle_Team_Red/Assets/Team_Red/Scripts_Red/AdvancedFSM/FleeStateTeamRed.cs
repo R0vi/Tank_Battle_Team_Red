69aff77c84fce012e0b5ab5d8f186f6537ac4e40
Ivor <ism.staats@student.han.nl>
2020-10-27 11:36:19 +0100

    fleestate move with navagent

--- Tank_Battle_Team_Red/Assets/Team_Red/Scripts_Red/AdvancedFSM/FleeStateTeamRed.cs
+++ Tank_Battle_Team_Red/Assets/Team_Red/Scripts_Red/AdvancedFSM/FleeStateTeamRed.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FleeStateTeamRed : FSMStateTeamRed
 {
@@ -14,10 +15,18 @@ public class FleeStateTeamRed : FSMStateTeamRed
     public override void ActTeamRed(Transform redTank, IList<Transform> platoonRedTanks, IList<Transform> enemyTanks)
     {
         Debug.Log("Flee state active");
-        var firstEnemyPosition = enemyTanks.First().position;
-        redTank.LookAt(firstEnemyPosition);
-        Quaternion.Slerp(redTank.rotation, Quaternion.Euler(0, 180, 0), 1 * Time.deltaTime);
-        redTank.Translate(Vector3.forward * Time.deltaTime * curSpeed);
+
+        Vector3 inverseAgentDirection = redTank.position - enemyTanks.First().position;
+
+        if (inverseAgentDirection.magnitude != 0)
+            destPos = inverseAgentDirection.normalized / inverseAgentDirection.magnitude;
+
+        redTank.gameObject.GetComponent<NavMeshAgent>().SetDestination(destPos);
+
+        //var firstEnemyPosition = enemyTanks.First().position;
+        //redTank.LookAt(firstEnemyPosition);
+        //Quaternion.Slerp(redTank.rotation, Quaternion.Euler(0, 180, 0), 1 * Time.deltaTime);
+        //redTank.Translate(Vector3.forward * Time.deltaTime * curSpeed);
     }
 
     public override void ReasonTeamRed(Transform redTank, IList<Transform> platoonRedTanks, IList<Transform> enemyTanks)
